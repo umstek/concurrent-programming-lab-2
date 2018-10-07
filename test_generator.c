928353bd5fffc5fb7b9672667a6de70648812fb8
Wickramaranga Abeygunawardhana <umstek@live.com>
2018-10-07 21:41:25 +0530

    Fix memset issue

--- test_generator.c
+++ test_generator.c
@@ -5,19 +5,6 @@
 #define M 10000
 
 void random_fill(int *operations, const int mInserts, const int mDeletes) {
-//    int a;
-//    do a = rand() % M; while (M / a == 0);
-//    int b = rand() % M;
-//
-//    for (int i = 0; i < mInserts; ++i) {
-//        int ix = (a * i + b) % M;
-//        operations[ix] = 1;
-//    };
-//    for (int j = mInserts; j < mInserts + mDeletes; ++j) {
-//        int jx = (a * j + b) % M;
-//        operations[jx] = 2;
-//    };
-
     for (int k = 0; k < mInserts; ++k) {
         int r = rand() % M;
         if (operations[r] == 0) operations[r] = 1; else k--;
@@ -36,6 +23,6 @@ void populate_values(int *values) {
 }
 
 void populate_operations(int *operations, int mInserts, int mDeletes) {
-    memset(operations, 0, sizeof(*operations));
+    memset(operations, 0, M * sizeof(operations[0]));
     random_fill(operations, mInserts, mDeletes);
 }
